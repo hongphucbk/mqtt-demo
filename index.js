c3e7ce1bbaec9a29786ee519366f78bbc57b4deb
hongphucbk <phuchong94@gmail.com>
2019-08-15 19:45:41 +0700

    add user list

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+var engine = require('ejs-locals');
+app.engine('ejs', engine);
+
+
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.render('layout/index');
+}) 
+
+app.get('/users', function(req, res) {
+	res.render('users/list');
 }) 
 
 app.listen(port, function(){
